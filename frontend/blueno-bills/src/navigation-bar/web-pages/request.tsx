6e1d0d64652872b177f662161439c4eb3719a5eb
Josh Benzon <benzonjosh@gmail.com>
2022-04-26 16:47:55 -0400

    feat: added inputs for transfer sub page

--- frontend/blueno-bills/src/navigation-bar/web-pages/request.tsx
+++ frontend/blueno-bills/src/navigation-bar/web-pages/request.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Button from '../../buttons/components/buttons'
 
 const Request = () => {
+
     return (
         <div
             style={{
--- frontend/blueno-bills/src/navigation-bar/web-pages/transferMeal.tsx
+++ frontend/blueno-bills/src/navigation-bar/web-pages/transferMeal.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import Button from "../../buttons/components/buttons";
+import {useForm} from "react-hook-form";
 
 const TransferMeal = () => {
+    const {register, handleSubmit, watch, formState: {errors}} = useForm();
+    const onSubmit = (data: any) => console.log(data);
+
     return (
         <div
             style={{
@@ -10,7 +15,61 @@ const TransferMeal = () => {
                 height: '100vh'
             }}
         >
-            <h1>Transfer Meal Page!</h1>
+            <div className="request-info"
+                 style={{
+                     display: 'flex',
+                     padding: '10px'
+                 }}>
+
+                <h1>Transfer: Meal Credits</h1>
+            </div>
+
+            <form onSubmit={handleSubmit(onSubmit)}
+
+                  style={{
+                      display: 'flex',
+                      padding: '10px'
+                  }}>
+
+                <div>
+                    To Who:
+                    <input {...register("name",
+                        {required: true, maxLength: 20, pattern: /^[A-Za-z]+$/i})} />
+                    {errors.name && "To Who is Required!"}
+                </div>
+
+                <div>
+                    Description:
+                    <input {...register("description", {maxLength: 100})} />
+                </div>
+
+                <div>
+                    Amount:
+                    <input type="number" {...register("amount", {min: 0, max: 20})} />
+                    {errors.amount && "Amount Must Be in Bounds!"}
+                </div>
+
+                <div>
+                    <input type="submit"/>
+                </div>
+            </form>
+
+            <div className="transfer-meal-button"
+                 style={{
+                     display: 'flex',
+                     padding: '10px'
+                 }}>
+
+                <Button
+                    border="none"
+                    color="#4CC37B"
+                    height="100px"
+                    width="200px"
+                    radius="5%"
+                    children="Transfer"
+                    onClick={() => console.log("Transfer Meal Credit!")}
+                />
+            </div>
         </div>
     );
 };
