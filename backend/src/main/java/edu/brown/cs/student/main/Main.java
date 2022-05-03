8fafe6739210b11a58ad927fbb6c8dbfad0108cd
mendiratta27 <77818928+mendiratta27@users.noreply.github.com>
2022-05-03 11:00:59 -0400

    commit

--- backend/src/main/java/edu/brown/cs/student/main/Main.java
+++ backend/src/main/java/edu/brown/cs/student/main/Main.java
@@ -1,8 +1,32 @@
 package edu.brown.cs.student.main;
 
+
+import freemarker.template.Configuration;
 import joptsimple.OptionParser;
 import joptsimple.OptionSet;
+import org.eclipse.jetty.websocket.api.Session;
+import org.json.JSONObject;
+import spark.ModelAndView;
+import spark.Request;
+import spark.Response;
 import spark.Spark;
+import spark.TemplateViewRoute;
+import spark.template.freemarker.FreeMarkerEngine;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import static j2html.TagCreator.article;
+import static j2html.TagCreator.attrs;
+import static j2html.TagCreator.b;
+import static j2html.TagCreator.p;
+import static j2html.TagCreator.span;
 
 /**
  * The Main class of our project. This is where execution begins.
@@ -19,6 +43,7 @@ public final class Main {
    */
   public static void main(String[] args) {
     new Main(args).run();
+    runSparkServer(4567);
   }
 
   private final String[] args;
@@ -26,6 +51,9 @@ public final class Main {
   private Main(String[] args) {
     this.args = args;
   }
+
+  public static int nextUserNumber = 1; //Assign to username for next connecting user
+
 
   private void run() {
 
@@ -43,6 +71,10 @@ public final class Main {
   private static void runSparkServer(int port) {
     Spark.port(port);
     Spark.externalStaticFileLocation("src/main/resources/static");
+
+    FreeMarkerEngine engine = createEngine();
+    Spark.get("/ws", new Main.HomePage(), engine);
+    Spark.init();
 
     Spark.options("/*", (request, response) -> {
       String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
@@ -64,5 +96,52 @@ public final class Main {
     // Put Routes Here
     // Spark.get("/table", new TableHandler());
     Spark.init();
+  }private static FreeMarkerEngine createEngine() {
+    Configuration config = new Configuration(Configuration.DEFAULT_INCOMPATIBLE_IMPROVEMENTS);
+    File templates = new File("src/main/resources/public");
+    try {
+      config.setDirectoryForTemplateLoading(templates);
+    } catch (IOException ioe) {
+      System.out.printf("ERROR: Unable use %s for template loading.%n", templates);
+      System.exit(1);
+    }
+    return new FreeMarkerEngine(config);
+  }
+
+  //Sends a message from one user to all users, along with a list of current usernames
+  public static void broadcastMessage(String sender, String message, int room) {
+    List<Session> sessionsToSend = new ArrayList<>();
+    List<String> usersList = new ArrayList<>();
+
+
+
+    sessionsToSend.stream().filter(Session::isOpen).forEach(session ->{
+      try {
+        session.getRemote().sendString(String.valueOf(new JSONObject()
+            .put("userMessage", createHtmlMessageFromSender(sender, message))
+            .put("userlist", usersList)
+        ));
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    });
+
+  }
+
+  //Builds a HTML element with a sender-name, a message, and a timestamp,
+  private static String createHtmlMessageFromSender(String sender, String message) {
+    return article(
+        b(sender + " says:"),
+        span(attrs(".timestamp"), new SimpleDateFormat("HH:mm:ss").format(new Date())),
+        p(message)
+    ).render();
+  }
+
+  public static class HomePage implements TemplateViewRoute {
+    @Override
+    public ModelAndView handle(Request request, Response response) throws Exception {
+      // TODO Auto-generated method stub
+      return new ModelAndView(null, "websocket.ftl");
+    }
   }
 }
