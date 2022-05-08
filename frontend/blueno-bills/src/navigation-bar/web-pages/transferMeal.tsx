48e844013dd81d57c87edf20024b517098904de1
Josh Benzon <benzonjosh@gmail.com>
2022-05-08 18:23:49 -0400

    feat: fix email input bug

--- frontend/blueno-bills/src/navigation-bar/web-pages/transferMeal.tsx
+++ frontend/blueno-bills/src/navigation-bar/web-pages/transferMeal.tsx
@@ -18,14 +18,13 @@ interface TransferMealProp {
 }
 
 interface InputProp {
-  name: string | null;
+  email: string | null;
   description: string | null;
   amount: number | null;
 }
 
-
 function TransferMeal(props: TransferMealProp) {
-  const [inputEmail, setinputEmail] = useState<string >("");
+  const [inputEmail, setInputEmail] = useState<string>("");
   const [inputDescription, setInputDescription] = useState<string | null>(null);
   const [inputAmount, setInputAmount] = useState<string | null>(null);
 
@@ -43,70 +42,111 @@ function TransferMeal(props: TransferMealProp) {
 
   const onSubmit = (inputData: InputProp) => storeInputData();
 
-  const currUserEmail = "Jillian_Dominguez@brown.edu" //hard coding for now until we have auth established and can store curr user data
-  let currUserMealSwipes : number = 0;
-  let recipientCurrMealSwipes : number = 0; //the current number of meal swipes of the person being sent swipes
-  if(props.rows){
-    for(let i = 0; i<props.rows.length; i++){
-        if(equalsIgnoringCase(props.rows[i].email,currUserEmail)){//user
-    
-            currUserMealSwipes = parseInt(props.rows[i].mealSwipes)
-        }
-        if(equalsIgnoringCase(props.rows[i].email,inputEmail)){//recipient
+  const currUserEmail = "Jillian_Dominguez@brown.edu"; //hard coding for now until we have auth established and can store curr user data
+  let currUserMealSwipes: number = 0;
+  let recipientCurrMealSwipes: number = 0; //the current number of meal swipes of the person being sent swipes
+  if (props.rows) {
+    for (let i = 0; i < props.rows.length; i++) {
+      if (equalsIgnoringCase(props.rows[i].email, currUserEmail)) {
+        //user
 
-            recipientCurrMealSwipes = parseInt(props.rows[i].mealSwipes)
-        }
+        currUserMealSwipes = parseInt(props.rows[i].mealSwipes);
+      }
+      if (equalsIgnoringCase(props.rows[i].email, inputEmail)) {
+        //recipient
+
+        recipientCurrMealSwipes = parseInt(props.rows[i].mealSwipes);
+      }
     }
   }
 
   //from here: https://stackoverflow.com/questions/2140627/how-to-do-case-insensitive-string-comparison
-  function equalsIgnoringCase(text:string, other:string) {
-    return text.localeCompare(other, undefined, { sensitivity: 'base' }) === 0;
-}
-
-  let newNumMealSwipes : number = 0;
-  let newRecipientMealSwipes : number = 0;
-  if(inputAmount){
-    newNumMealSwipes = currUserMealSwipes - parseInt(inputAmount)
-    newRecipientMealSwipes = recipientCurrMealSwipes + parseInt(inputAmount)
-    console.log("new user meal swipes: " + newNumMealSwipes)
-    console.log("recipient new meal swipes: " + newRecipientMealSwipes)
+  function equalsIgnoringCase(text: string, other: string) {
+    return text.localeCompare(other, undefined, { sensitivity: "base" }) === 0;
   }
-  
+
+  let newNumMealSwipes: number = 0;
+  let newRecipientMealSwipes: number = 0;
+  if (inputAmount) {
+    newNumMealSwipes = currUserMealSwipes - parseInt(inputAmount);
+    newRecipientMealSwipes = recipientCurrMealSwipes + parseInt(inputAmount);
+    console.log("new user meal swipes: " + newNumMealSwipes);
+    console.log("recipient new meal swipes: " + newRecipientMealSwipes);
+  }
 
   const UpdateRequest = () => {
-
-    
     storeInputData();
     //here we are updating the mealSwipes column in the StudentData table
     //decrementing the current user's meal swipes by 1 since they are transferring
-    const body :string =  "{\"tableName\": " + "\"" + "StudentData" + "\"" + ", \"colNameToNewVal\" : {" +  "\"" +"mealSwipes" + "\"" + " : " 
-        + "\""+ newNumMealSwipes + "\"" + "}, \"conditions\" : {" 
-        + "\"" + "email" + "\"" + " : "  + "\"" + currUserEmail + "\""+ "}}"
+    const body: string =
+      '{"tableName": ' +
+      '"' +
+      "StudentData" +
+      '"' +
+      ', "colNameToNewVal" : {' +
+      '"' +
+      "mealSwipes" +
+      '"' +
+      " : " +
+      '"' +
+      newNumMealSwipes +
+      '"' +
+      '}, "conditions" : {' +
+      '"' +
+      "email" +
+      '"' +
+      " : " +
+      '"' +
+      currUserEmail +
+      '"' +
+      "}}";
 
-        const requestOptions = {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: body
-        };
-        console.log("req body: " + requestOptions.body)
-        fetch('http://localhost:4567/update', requestOptions)
-            .then(response => {response.json();console.log("response: " + response.status)})
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: body,
+    };
+    console.log("req body: " + requestOptions.body);
+    fetch("http://localhost:4567/update", requestOptions).then((response) => {
+      response.json();
+      console.log("response: " + response.status);
+    });
 
     //we need another POST request to update the values of the user who was sent the meal swipe
 
-    const recipientBody :string =  "{\"tableName\": " + "\"" + "StudentData" + "\"" + ", \"colNameToNewVal\" : {" +  "\"" +"mealSwipes" + "\"" + " : " 
-        + "\""+ newRecipientMealSwipes + "\"" + "}, \"conditions\" : {" 
-        + "\"" + "email" + "\"" + " : "  + "\"" + inputEmail + "\""+ "}}"
+    const recipientBody: string =
+      '{"tableName": ' +
+      '"' +
+      "StudentData" +
+      '"' +
+      ', "colNameToNewVal" : {' +
+      '"' +
+      "mealSwipes" +
+      '"' +
+      " : " +
+      '"' +
+      newRecipientMealSwipes +
+      '"' +
+      '}, "conditions" : {' +
+      '"' +
+      "email" +
+      '"' +
+      " : " +
+      '"' +
+      inputEmail +
+      '"' +
+      "}}";
 
-        const requestOptionsRecipient = {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: body
-        };
-        console.log("req body: " + requestOptions.body)
-        fetch('http://localhost:4567/update', requestOptions)
-            .then(response => {response.json();console.log("response: " + response.status)})
+    const requestOptionsRecipient = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: body,
+    };
+    console.log("req body: " + requestOptions.body);
+    fetch("http://localhost:4567/update", requestOptions).then((response) => {
+      response.json();
+      console.log("response: " + response.status);
+    });
   };
 
   return (
@@ -122,18 +162,16 @@ function TransferMeal(props: TransferMealProp) {
 
             <input
               type="text"
-              placeholder="email"
-              {...register("name", {
+              placeholder="Email"
+              {...register("email", {
                 required: true,
-                maxLength: 20,
-                pattern: /^[A-Za-z]+$/i,
+                maxLength: 50,
               })}
               onChange={(event: React.ChangeEvent<HTMLInputElement>): void =>
-                setinputEmail
-            (event.target.value)
+                setInputEmail(event.target.value)
               }
             />
-            <div className="error-message">{errors.name && "Required!"}</div>
+            <div className="error-message">{errors.email && "Required!"}</div>
           </div>
 
           <div className="input-group">
@@ -172,7 +210,6 @@ function TransferMeal(props: TransferMealProp) {
       </div>
     </React.Fragment>
   );
-
 }
 
 export default TransferMeal;
